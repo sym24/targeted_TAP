34d1d459042f1c526cc5568dd37bb732ab20502f
Yi-Ming Sun <yimingsun199324@gmail.com>
2015-04-23 20:14:00 -0700

    biobloom config

---
+++ biobloom_profile.sh
@@ -0,0 +1,4 @@
+export BIN_PATH=/home/ymingsun/Install/centos5/bbt-2.0.11/bin/:/projects/btl/arch/
+export PYTHONPATH=/projects/btl/trans-abyss/public_releases/transabyss-1.5.2-centos5/prereqs/Python-2.7.8
+export PATH=${PYTHONPATH}:${BIN_PATH}:$PATH
+# source BioBloomTools installation dictionary
