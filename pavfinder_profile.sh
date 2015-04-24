ab201312cc9d2e32dffd464e12171afadcc741bb
Yi-Ming Sun <yimingsun199324@gmail.com>
2015-04-23 20:15:01 -0700

    pavfinder

---
+++ pavfinder_profile.sh
@@ -0,0 +1,5 @@
+export PAVFINDER_PATH=/projects/btl/pavfinder/v0.1.0 
+export GMAP_PATH=/projects/btl/arch/gmap-2014-01-21/bin
+export BIN_PATH=/projects/btl/arch/ 
+export PYTHONPATH=/projects/btl/arch/Python-2.7.5/bin:/projects/btl/arch/Python-2.7.5/lib/python2.7/site-packages/:${PAVFINDER_PATH}
+export PATH=${PYTHONPATH}:${BIN_PATH}:${GMAP_PATH}:$PATH
